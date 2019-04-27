dd8d6e3761a5276e82430ee2713dc7ecede14510
Misha <zielinskanoemi5@gmail.com>
2019-04-27 12:05:04 +0200

    Init commit

---
+++ src/main/java/pl/kocie_stopki/kocie/KocieApplication.java
@@ -0,0 +1,13 @@
+package pl.kocie_stopki.kocie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KocieApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KocieApplication.class, args);
+    }
+
+}
