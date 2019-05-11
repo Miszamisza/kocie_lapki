28afe16d23bce952022f61989dc7c008448925c8
KamilCzerniak <kamil.z.czerniak@gmail.com>
2019-05-11 14:16:12 +0200

    Added missed files from frontend

--- frontend/src/components/hoc/Layout.js
+++ frontend/src/components/hoc/Layout.js
@@ -1,15 +1,17 @@
 import React, {Component} from 'react';
-
-
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from '../header/Header';
 
 class Layout extends Component {
 
-
     render() {
         return (
-            <div>
-
-            </div>
+            <>
+                <Header/>
+                <main>
+                    {this.props.children}
+                </main>
+            </>
         );
     }
 }
